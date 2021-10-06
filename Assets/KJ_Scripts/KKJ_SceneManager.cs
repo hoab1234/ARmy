f7a9e8045a85c0897b4a2d471a3de67d39dd5326
kkj <55876137+hoab1234@users.noreply.github.com>
2021-10-06 10:07:28 +0900

    scene Manager add

--- Assets/KJ_Scripts/KKJ_SceneManager.cs
+++ Assets/KJ_Scripts/KKJ_SceneManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class KKJ_SceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void GoToScene00()
     {
-        
+        SceneManager.LoadScene(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToScene01()
     {
-        
+        SceneManager.LoadScene(1);
     }
+
+    public void GoToScene02()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
 }
